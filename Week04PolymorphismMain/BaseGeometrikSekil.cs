545d6ad2e75d282bd6085626b068b11c7700baec
ilknurdenizb <ideniz.baloglu@gmail.com>
2025-07-20 16:56:59 +0300

    Add project files.

---
+++ Week04PolymorphismMain/BaseGeometrikSekil.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week04PolymorphismMain
+{
+    public class BaseGeometrikSekil
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+
+        public BaseGeometrikSekil(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public virtual double AreaCalculator()
+        {
+            return Height * Width;
+        }
+
+        public virtual string ShapeName => "Bilinmeyen Şekil";
+
+        public virtual void DisplayArea()
+        {
+            Console.WriteLine($"{ShapeName} Alanı: {AreaCalculator()}");
+        }
+    }
+
+    // Kare sınıfı
+    public class Square : BaseGeometrikSekil
+    {
+        public Square(double height, double width) : base(height, width) { }
+
+        public override string ShapeName => "Kare";
+
+        public override void DisplayArea()
+        {
+            if (Height != Width)
+            {
+                Console.WriteLine("Uyarı: Karenin kenarları eşit olmalı!");
+            }
+            base.DisplayArea();
+        }
+    }
+
+    // Dikdörtgen sınıfı
+    public class Rectangle : BaseGeometrikSekil
+    {
+        public Rectangle(double height, double width) : base(height, width) { }
+
+        public override string ShapeName => "Dikdörtgen";
+    }
+
+    // Dik Üçgen sınıfı
+    public class RightTriangle : BaseGeometrikSekil
+    {
+        public RightTriangle(double height, double width) : base(height, width) { }
+
+        public override double AreaCalculator()
+        {
+            return 0.5 * Height * Width;
+        }
+
+        public override string ShapeName => "Dik Üçgen";
+    }
+}
+
---
+++ Week04PolymorphismMain/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Week04PolymorphismMain;
+class Program
+{
+    static void Main()
+    {
+        BaseGeometrikSekil square = new Square(4, 4);
+        BaseGeometrikSekil rectangle = new Rectangle(5, 12);
+        BaseGeometrikSekil rightTriangle = new RightTriangle(5, 10);
+
+        square.DisplayArea();       // 16
+        rectangle.DisplayArea();    // 60
+        rightTriangle.DisplayArea(); // 25
+    }
+}
